df9a0db8fd482ded41b510684c2e25b10b64115e
rohitbhalke <bhalker@pun-bhalker-m.ds.ad.adp.com>
2016-08-21 18:32:59 +0530

    added console.log for db connectivity

---
+++ config/database.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bhalker on 29/12/15.
+ */
+
+
+<!-- will hold our database connection settings -->
+
+module.exports = {
+
+    //'url' : 'mongodb://localhost/databaseNotes' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://rohit:rohit@ds013216.mlab.com:13216/privatediary'
+};
